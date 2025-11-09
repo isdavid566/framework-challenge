592e94fc513aba0fc07bf6f9a9cc80670d7de229
isdavid566 <isdavid566@gmail.com>
2025-11-09 00:10:39 +0000

    Updated CSS

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// Source - https://stackoverflow.com/a
+// Posted by Danila, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-11-08, License - CC BY-SA 4.0
+
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        // you can either spread `colors` to apply all the colors
+        ...colors,
+        // or add them one by one and name whatever you want
+        amber: colors.amber,
+        emerald: colors.emerald,
+      }
+    }
+  }
+}
